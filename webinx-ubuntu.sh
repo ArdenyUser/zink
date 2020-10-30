c0058931d9ba7e0b2f06bfd1ba9311be3b181e64
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2020-10-30 13:44:42 -0400

    main

---
+++ webinx-ubuntu.sh
@@ -0,0 +1,5 @@
+# Web browsing pro with terminal. (FOR LINUX)
+echo For Linux Suppoted Systems ONLY! Mainly UBUNTU.
+sudo apt-get update -y
+sudo apt-get install -y links2
+echo Completed Installion
