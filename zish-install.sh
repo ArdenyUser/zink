c7a284fd29843760c402eeeaa72e6794ca24c866
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2020-10-30 13:29:21 -0500

    main

---
+++ zish-install.sh
@@ -0,0 +1,9 @@
+echo "Zish Official Installer"
+echo "Enter the program's current directory :"
+read dira
+echo "Enter destinion of the program :"
+read dird
+echo "Installing fully..."
+mv $dira $dird
+echo "Fully installed program!"
+echo "Location is $dird "
